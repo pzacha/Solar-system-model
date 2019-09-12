0f6289800d80ad3c2d2f914aa9c530ca77bf8e8d
zacha <pzacha11@gmail.com>
2019-09-12 10:36:37 +0200

    Animation improved.

--- PythonApp/Mass.py
+++ PythonApp/Mass.py
@@ -11,7 +11,7 @@ list_of_objects = np.empty(0, dtype = object)
 # Timestamp
 timestamp = 600
 # Screen max width and height (screen is square)
-screen_size = 1000
+screen_size = 640
 max_dist = 10 ** 12
 # Orbits (0 -> off, 1 -> on)
 orbits = 1
@@ -62,12 +62,28 @@ class mass:
         self.ycor = self.ycor + self.yvel * timestamp
 
 def norm_coords(coord):
+    """ Adjust coordinates to screen (0,0) = middle of the screen """
     if coord == 0:
+        # Middle of the screen
         coord = screen_size/2
     elif coord >= 0:
-        coord = screen_size/2 + coord/max_dist*screen_size/2
+        # Right/top side of the screen
+        # If absolute distance is bigger than max_dist show the planet on the edge of the screen
+        if abs(coord) > max_dist:
+            coord = screen_size
+        else:
+            coord = screen_size/2 + coord/max_dist*screen_size/2
     else:
-       coord = screen_size/2 - abs(coord)/max_dist*screen_size/2
+        # Left/bottom side of the screen
+        # If absolute distance is bigger than max_dist show the planet on the edge of the screen
+        if abs(coord) > max_dist:
+            # If distance is bigger than max_dist show the planet on the edge of the screen
+            coord = 0
+        else:
+            
+            coord = screen_size/2 - abs(coord)/max_dist*screen_size/2
+        
+       
     return int(round(coord))
 
 earth = mass(5.972 * (10 ** 24), 149600000000, 0, 0, 30000, 0, 0)
@@ -82,29 +98,28 @@ for i in range(2):
 
 # Animation
 pygame.init()
-screen = pygame.display.set_mode((1000,1000))
+screen = pygame.display.set_mode((screen_size,screen_size))
 pygame.display.update()
 
 iter = 0
 for time in range(0, timestamp*365*24*6*2, timestamp):
-    if iter == 20:
+    if iter == 100:
+        pygame.display.update()
+        screen.fill((0,0,0))
         iter = 0
     iter = iter + 1
-    # Refresh screen
-    if iter == 15 and orbits == 0:
-        screen.fill((0,0,0))
     for obj in list_of_objects:
         # Calculate accelerartions
         obj.calc_acceleration()
         # Update properties of each planet
         obj.update_velocity_and_coordinates()
-        # Update screen every 10 iterations
-        if iter == 15:
+        # Update screen every iter iterations
+        if iter == 100:
             if obj == sun:
                 pygame.draw.circle(screen, (255,255,0), (norm_coords(sun.xcor),norm_coords(sun.ycor)), 10)
             elif obj == earth:
                 pygame.draw.circle(screen, (0,255,0), (norm_coords(obj.xcor),norm_coords(obj.ycor)), 4)
             else:
                 pygame.draw.circle(screen, (255,255,255), (norm_coords(obj.xcor),norm_coords(obj.ycor)), 3)
-            pygame.display.update()
+            
     
