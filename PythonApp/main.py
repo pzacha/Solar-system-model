a3e24ba6b9100bd85d2af0b342eedf11e540c8c6
zacha <pzacha11@gmail.com>
2019-10-11 08:46:11 +0200

    test2

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -44,4 +44,3 @@ conn.close()
 #TODO cursor.execute("INSERT INTO table VALUES (%s, %s, %s)", (var1, var2, var3))
 # 1. Create variables for saving coordinates
 # 2. Save coordinates to SQL db (pickle?)
-#
